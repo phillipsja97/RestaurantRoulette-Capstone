9fe714d6c9c64cfa960684656b5014ad34af892a
Jamie Phillips <phillipsja97@gmail.com>
2020-07-10 19:08:45 -0500

    top half of auth screen set up with login and register UI

--- restaurantroulette.ui/src/Components/Pages/Home/Home.js
+++ restaurantroulette.ui/src/Components/Pages/Home/Home.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'antd';
+import { Button, Card, Divider } from 'antd';
+import './Home.scss';
+
+
 
 export default function Home(props) {
   return (
     <div className="home">
+      <div className="loginPictureForm">
+        <div className="loginCardContainer">
+          <Card style={{ width: 600, backgroundColor: 'gray' }} className="loginCard">
+            <div className="loginCardContent">
+              <div className="signInWithGoogle">
+                <Button ghost>Sign In With Google Account</Button>
+              </div>
+                <Divider>Or</Divider>
+              <div className="registerForAccount">
+                <Button ghost>Register For An Account</Button>
+              </div>
+            </div>
+          </Card>
+         </div>
+      </div>
       { (props.authed) ? <h1>authed in home</h1> : <h1>not authed in home</h1> }
       <Link to={'/swipe'}><Button>Swipe</Button></Link>
        </div>
