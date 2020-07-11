0a2fd435a0d8c6297ccd5f52a3ca9a8c24d157ec
Jamie Phillips <phillipsja97@gmail.com>
2020-07-11 17:14:43 -0500

    added a method to be able to sign up a new user

--- RestaurantRoulette-Capstone/Models/Users.cs
+++ RestaurantRoulette-Capstone/Models/Users.cs
@@ -8,11 +8,18 @@ namespace RestaurantRoulette_Capstone.Models
     public class Users
     {
         public int ID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string FullName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string FirebaseUID { get; set; }
         
+    }
+
+    public class NewUser
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string FirebaseUID { get; set; }
     }
 }
